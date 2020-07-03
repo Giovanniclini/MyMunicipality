6ec142b9a50ef51937058b2d2ecd10b4ed6836ae
Giovanniclini <gioviclini@gmail.com>
2020-07-03 20:29:45 +0200

    First commit (3)

---
+++ app/src/main/java/com/example/mymunicipality/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.mymunicipality;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        findViewById(R.id.buttonLogin).setOnClickListener(this);
+        findViewById(R.id.buttonRegistration).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
--- app/src/main/java/com/example/mymunicipality/MainActivity.java
+++ app/src/main/java/com/example/mymunicipality/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.mymunicipality;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final int LOGIN_REQUEST = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivityForResult(intent, LOGIN_REQUEST);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
