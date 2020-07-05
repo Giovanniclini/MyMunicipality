4d58d6d425cf0949509e1cd5b4f9b747e2530e94
Giovanniclini <gioviclini@gmail.com>
2020-07-05 18:45:56 +0200

    Aggiunta schermata di Homepage

--- app/src/main/java/com/example/mymunicipality/LoginActivity.java
+++ app/src/main/java/com/example/mymunicipality/LoginActivity.java
@@ -7,16 +7,21 @@ import android.view.View;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        findViewById(R.id.buttonLogin).setOnClickListener(this);
-        findViewById(R.id.buttonRegistration).setOnClickListener(this);
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_login);
+                findViewById(R.id.buttonLogin).setOnClickListener(this);
+                findViewById(R.id.buttonRegistration).setOnClickListener(this);
 
-    @Override
-    public void onClick(View v) {
+        }
 
-    }
-}
+
+
+
+
+        @Override
+        public void onClick(View v) {
+
+        }
+        }
--- app/src/main/java/com/example/mymunicipality/MainActivity.java
+++ app/src/main/java/com/example/mymunicipality/MainActivity.java
@@ -1,22 +1,36 @@
 package com.example.mymunicipality;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final int LOGIN_REQUEST = 100;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivityForResult(intent, LOGIN_REQUEST);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+     Intent intent = new Intent(this, LoginActivity.class);
+     startActivityForResult(intent, LOGIN_REQUEST);
     }
 
     @Override
