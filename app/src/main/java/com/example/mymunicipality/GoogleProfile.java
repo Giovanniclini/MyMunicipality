8d44fe3e299e412c5e11aac48e70723037b7c8b2
Giovanni Alessandro Clini <gioviclini@gmail.com>
2020-07-08 16:46:40 +0200

    Inserimento Bottom Navigation View e piccole migliorie

--- app/src/main/java/com/example/mymunicipality/GoogleProfile.java
+++ app/src/main/java/com/example/mymunicipality/GoogleProfile.java
@@ -1,30 +1,48 @@
 package com.example.mymunicipality;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class GoogleProfile extends AppCompatActivity {
 
     TextView name, email;
-    Button logout;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_profile);
 
-        logout = findViewById(R.id.google_logout);
+        BottomNavigationView navigation = findViewById(R.id.NavView);
+        navigation.getMenu().clear(); //clear old inflated items.
+        navigation.inflateMenu(R.menu.bott_nav_menu);
+
         name = findViewById(R.id.name);
         email = findViewById(R.id.email);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
         if(signInAccount != null){
@@ -32,13 +50,11 @@ public class GoogleProfile extends AppCompatActivity {
             email.setText(signInAccount.getEmail());
         }
 
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+    }
+
+    public void menuEdit(MenuItem item) {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/mymunicipality/MainActivity.java
+++ app/src/main/java/com/example/mymunicipality/MainActivity.java
@@ -8,35 +8,30 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends AppCompatActivity  {
 
     public static final int LOGIN_REQUEST = 100;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar myToolbar = findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-        BottomNavigationView navigation = findViewById(R.id.NavView);
-        navigation.getMenu().clear(); //clear old inflated items.
-        navigation.inflateMenu(R.menu.bott_nav_menu);
-
 
         Intent intent = new Intent(this, LoginActivity.class);
         startActivityForResult(intent, LOGIN_REQUEST);
+
     }
 
 }
+
