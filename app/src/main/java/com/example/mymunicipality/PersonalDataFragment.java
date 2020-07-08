4b18425a4fb0448078bc283c4c0b2cd0fc81a895
LolloVagnoloo <57802876+LolloVagnoloo@users.noreply.github.com>
2020-07-08 18:37:46 +0200

    Inserimento Fragment Area Perosnale (da riempire con elementi)

---
+++ app/src/main/java/com/example/mymunicipality/PersonalDataFragment.java
@@ -0,0 +1,32 @@
+package com.example.mymunicipality;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+
+public class PersonalDataFragment extends Fragment {
+
+    private TextView textView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_personal_data, container, false);
+
+        return  view;
+
+    }
+
+}
