e8c4fd240d80778416b41c09013305183fe057ec
Giovanniclini <gioviclini@gmail.com>
2020-07-14 17:14:56 +0200

    Facebook login quasi completato(1)

--- app/src/main/java/com/example/mymunicipality/PersonalDataFragment.java
+++ app/src/main/java/com/example/mymunicipality/PersonalDataFragment.java
@@ -1,26 +1,21 @@
 package com.example.mymunicipality;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.facebook.Profile;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Objects;
-
-import static android.content.Intent.getIntent;
 
 
 public class PersonalDataFragment extends Fragment {
@@ -46,13 +41,10 @@ public class PersonalDataFragment extends Fragment {
         email.setText(email1);
 
 
-        /*FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             //name.setText(Objects.requireNonNull(user.getDisplayName()));
             email.setText(user.getEmail());
-        }
-        else{
-            Toast.makeText(getContext(), "You're not logged in", Toast.LENGTH_SHORT).show();
         }
 
         GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(Objects.requireNonNull(getActivity()));
@@ -61,7 +53,7 @@ public class PersonalDataFragment extends Fragment {
             email.setText(signInAccount.getEmail());
         }
 
-         */
+
 
         return  view;
     }
@@ -74,12 +66,6 @@ public class PersonalDataFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-    }
-
-    public void sendData(String data1, String data2) {
-        if(data1 != null && data2 != null)
-            name.setText(data1);
-            email.setText(data2);
     }
 
 }
