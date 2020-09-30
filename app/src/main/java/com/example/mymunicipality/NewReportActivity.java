6cad51a2f4006cb14a0652e8cd59956a907b29b1
LolloVagnoloo <57802876+LolloVagnoloo@users.noreply.github.com>
2020-09-30 18:29:10 +0200

    Continuo aggiunta segnalazioni Modifiche al layout e in parte al java

--- app/src/main/java/com/example/mymunicipality/NewReportActivity.java
+++ app/src/main/java/com/example/mymunicipality/NewReportActivity.java
@@ -1,25 +1,42 @@
 package com.example.mymunicipality;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.imageview.ShapeableImageView;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class NewReportActivity extends AppCompatActivity {
 
+    private static final int PICK_IMAGE_REQUEST = 1;
     private EditText editTextTitle;
     private EditText editTextDescription;
     private EditText editTextPriority;
+    private ShapeableImageView photo_report;
+    private MaterialButton add_photo_button;
+    private Uri mImageUri;
+    private StorageReference mStorageRef;
     //private NumberPicker numberPickerPriority;
 
     @Override
@@ -29,15 +46,51 @@ public class NewReportActivity extends AppCompatActivity {
 
         editTextTitle = findViewById(R.id.edit_text_title);
         editTextDescription = findViewById(R.id.edit_text_description);
-        editTextPriority =findViewById(R.id.priority_number);
+        editTextPriority = findViewById(R.id.priority_number);
+        add_photo_button = findViewById(R.id.add_photo);
+        photo_report = findViewById(R.id.photo_report);
 
-        //numberPickerPriority = findViewById(R.id.number_picker_priority);
-
-        //numberPickerPriority.setMinValue(1);
-        //numberPickerPriority.setMaxValue(10);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+        add_photo_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFileChooser();
+            }
+        });
+    }
+
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        mImageUri = data.getData();
+        Picasso.get().load(mImageUri).into(photo_report);
+        String reportPictures = "reportPictures/";
+        String path = reportPictures + editTextTitle.getText();
+        StorageReference reverseRef = mStorageRef.child(path);
+        reverseRef.putFile(mImageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
     }
 
     @Override
