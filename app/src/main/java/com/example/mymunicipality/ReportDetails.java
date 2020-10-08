563c5e77fb8a6d40a6fc7f5c8b90b9eab0ba4a0e
Giovanniclini <gioviclini@gmail.com>
2020-10-08 17:52:07 +0200

    Aggiunta la foto in ReportDetails

--- app/src/main/java/com/example/mymunicipality/ReportDetails.java
+++ app/src/main/java/com/example/mymunicipality/ReportDetails.java
@@ -1,18 +1,38 @@
 package com.example.mymunicipality;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.imageview.ShapeableImageView;
 import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ReportDetails extends AppCompatActivity {
 
+    private static final String TAG = "ActivityDetails: ";
     MaterialTextView textViewStreet;
     MaterialTextView textViewDescription;
     MaterialTextView textViewPriority;
+    MaterialTextView textViewUser;
     ShapeableImageView imageViewReport;
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +42,66 @@ public class ReportDetails extends AppCompatActivity {
         textViewStreet = findViewById(R.id.view_street);
         textViewDescription = findViewById(R.id.view_description);
         textViewPriority = findViewById(R.id.view_priority);
-
+        textViewUser = findViewById(R.id.view_user);
         imageViewReport = findViewById(R.id.photo_report);
+
+        Intent intent = getIntent();
+        final String title = intent.getStringExtra("titolo");
+
+        if (title != null){
+            FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
+            DocumentReference mFirestoreDetails = mFirestore.collection("Reports").document(title);
+            mFirestoreDetails.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()){
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()){
+                            Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+
+                            String street = document.getString("via");
+                            String description = document.getString("description");
+                            String priority = String.valueOf(document.get("priority"));
+                            String user = document.getString("username");
+                            textViewStreet.setText(street);
+                            textViewPriority.setText(priority);
+                            textViewDescription.setText(description);
+                            textViewUser.setText(user);
+
+                            mStorageRef = FirebaseStorage.getInstance().getReference();
+                            String reportPictures = "reportPictures/";
+                            String path = reportPictures + title;
+                            StorageReference reportRef = mStorageRef.child(path);
+                            File localFile = null;
+                            try {
+                                localFile = File.createTempFile("images", "jpg");
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            final File finalLocalFile = localFile;
+                            reportRef.getFile(localFile)
+                                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                                        @Override
+                                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                                            // Successfully downloaded data to local file
+                                            Picasso.get().load(finalLocalFile).into(imageViewReport);
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception exception) {
+                                    // Handle failed download
+                                    // ...
+                                }
+                            });
+
+                        } else {
+                            Log.d(TAG, "No such document");
+                        }
+                    } else {
+                        Log.d(TAG, "get failed with", task.getException());
+                    }
+                }
+            });
+        }
     }
 }
