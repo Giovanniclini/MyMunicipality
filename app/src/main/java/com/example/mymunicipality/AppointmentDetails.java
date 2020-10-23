114f7de148cdb7209b803ff4d60deb81c711d27b
LolloVagnoloo <57802876+LolloVagnoloo@users.noreply.github.com>
2020-10-23 18:02:45 +0200

    Codice commentato per fare il calendario (?)

--- app/src/main/java/com/example/mymunicipality/AppointmentDetails.java
+++ app/src/main/java/com/example/mymunicipality/AppointmentDetails.java
@@ -3,13 +3,21 @@ package com.example.mymunicipality;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -21,12 +29,15 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textview.MaterialTextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Calendar;
 
 public class AppointmentDetails extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -35,6 +46,7 @@ public class AppointmentDetails extends AppCompatActivity implements OnMapReadyC
     MaterialTextView objectTextView;
     MaterialTextView dataoraTextView;
     MaterialTextView viaTextView;
+    MaterialButton calendarButton;
 
     String username1;
     String object1;
@@ -46,6 +58,7 @@ public class AppointmentDetails extends AppCompatActivity implements OnMapReadyC
         inflater.inflate(R.menu.report_menu, menu);
         return  true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -89,6 +102,7 @@ public class AppointmentDetails extends AppCompatActivity implements OnMapReadyC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment_details);
 
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -97,7 +111,14 @@ public class AppointmentDetails extends AppCompatActivity implements OnMapReadyC
         objectTextView = findViewById(R.id.object_text_camp);
         dataoraTextView = findViewById(R.id.dataora_text_camp);
         viaTextView = findViewById(R.id.via_text_camp);
+        calendarButton = findViewById(R.id.calendar_button);
 
+        calendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //addOnCalendar();
+            }
+        });
 
 
         Intent intent = getIntent();
@@ -163,6 +184,8 @@ public class AppointmentDetails extends AppCompatActivity implements OnMapReadyC
             });
         }
 
+
+
     }
 
     @Override
@@ -172,4 +195,62 @@ public class AppointmentDetails extends AppCompatActivity implements OnMapReadyC
                 .position(sydney)
                 .title("Marker in Sydney"));
     }
+
+  /*
+    String ora = splitHour(ora1);
+    String minuti = splitMinute(ora1);
+
+    String anno = splitYear(data1);
+    String mese = splitMonth(data1);
+    String giorno = splitDay(data1);
+
+    public static void addOnCalendar(){
+
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(anno, 10, 23, 7, 30);
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2020, 10, 23, 8, 30);
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
+                .putExtra(CalendarContract.Events.TITLE, "Appuntamento Pubblica Amministrazione")
+                .putExtra(CalendarContract.Events.DESCRIPTION, object1)
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, sector1)
+                .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY);
+        startActivity(intent);
+    }
+
+    public static String splitHour(String orario){
+        String[] parts = orario.split(" : ");
+        String ora = parts[0];
+        return ora;
+    }
+
+    public static String splitMinute(String orario){
+        String[] parts = orario.split(" : ");
+        String minuti = parts[1];
+        return minuti;
+    }
+
+    public static String splitYear(String data){
+        String[] parts = data.split("/");
+        String year = parts[2]; // 004
+       return year;
+    }
+
+    public static String splitMonth(String data){
+        String[] parts = data.split("/");
+        String month = parts[1];
+        return month;
+    }
+
+    public static String splitDay(String data){
+        String[] parts = data.split("/");
+        String day = parts[0];
+        return day;
+    }
+
+    */
+
 }
