f6795bec69cedb46f102a3cfde4dcd27fc46940e
LolloVagnoloo <57802876+LolloVagnoloo@users.noreply.github.com>
2020-10-26 16:34:12 +0100

    Splash View iniziale pronta

--- app/src/main/java/com/example/mymunicipality/SplashViewActivity.java
+++ app/src/main/java/com/example/mymunicipality/SplashViewActivity.java
@@ -2,6 +2,7 @@ package com.example.mymunicipality;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,9 +17,20 @@ public class SplashViewActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_view_activity);
 
-
+/*
         Intent intent = new Intent(this, LoginActivity.class);
         startActivityForResult(intent, LOGIN_REQUEST);
+        */
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashViewActivity.this,LoginActivity.class);
+                startActivityForResult(i,LOGIN_REQUEST);
+            }
+        }, 2000);
 
     }
 
