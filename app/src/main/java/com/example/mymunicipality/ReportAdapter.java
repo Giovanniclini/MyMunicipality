ab8f564933de8f3e221c6b7299e2a692ff9700ea
LolloVagnoloo <57802876+LolloVagnoloo@users.noreply.github.com>
2020-11-23 13:50:29 +0100

    try

--- app/src/main/java/com/example/mymunicipality/ReportAdapter.java
+++ app/src/main/java/com/example/mymunicipality/ReportAdapter.java
@@ -52,7 +52,7 @@ public class ReportAdapter extends FirestoreRecyclerAdapter<ReportData, ReportAd
                 FirebaseFirestore db = FirebaseFirestore.getInstance();
                 db
                         .collection("Votes")
-                        .document(username + " " + title)
+                        .document(title + " " + username)
                         .update("votesCount", votesCount);
 
                 if(votesCount >= -1 && votesCount <= 1){
@@ -78,7 +78,7 @@ public class ReportAdapter extends FirestoreRecyclerAdapter<ReportData, ReportAd
                 FirebaseFirestore db = FirebaseFirestore.getInstance();
                 db
                         .collection("Votes")
-                        .document(username + " " + title)
+                        .document(title + " " + username)
                         .update("votesCount", votesCount);
 
                 if(votesCount >= -1 && votesCount <= 1){
